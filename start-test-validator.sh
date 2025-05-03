b1de1338e233e64dd5e7d7cf93801bcda86606af
nvnx7 <thenvnsahu@gmail.com>
2025-05-03 17:31:53 +0530

    feat: add script to spin-up test validator

---
+++ start-test-validator.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+solana-test-validator \
+  --reset \
+  --url https://api.mainnet-beta.solana.com \
+  --clone-upgradeable-program rec5EKMGg6MxZYaMdyBfgwp4d5rB9T1VQH5pJv5LtFJ \
+  --clone-upgradeable-program pythWSnswVUd12oZpeFP8e9CVaEqJg25g1Vtc2biRsT \
+  --clone-upgradeable-program HDwcJBJXjL9FpJ7UBsYBtaDjsBUhuLCUYoz3zr8SWWaQ \
+  --clone DBE3N8uNjhKPRHfANdwGvCZghWXyLPdqdSbEW2XFwBiX \
+  --clone 6UxPR2nXJNNM1nESVWGAf8NXMVu3SGgYf3ZfUFoGB9cs \
+  --clone 6B23K3tkb51vLZA14jcEQVCA1pfHptzEHFA93V5dYwbT \
+  --clone 9vNb2tQoZ8bB4vzMbQLWViGwNaDJVtct13AGgno1wazp \
+  --clone 5gxPdahvSzcKySxXxPuRXZZ9s6h8hZ88XDVKavWpaQGn \
+  --clone DaWUKXCyXsnzcvLUyeJRWou8KTn7XtadgTsdhJ6RHS7b
